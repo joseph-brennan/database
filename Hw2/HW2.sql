a892eba0a809afe89af399f9a406b6b65ca81ac1
Joey <sharpshooter30@gmail.com>
2017-09-07 17:03:57 -0600

    starting to finish up

--- Hw2/HW2.sql
+++ Hw2/HW2.sql
@@ -2,9 +2,10 @@
 * Joey Brennan
 *
 */
+\o "C:\Users\Joey\Documents\Database\Hw2\HW2.out";
 
 CREATE TABLE Countries (
-    name VARCHAR(20)
+    name VARCHAR(20) PRIMARY KEY,
     latitude INT,
     longitude INT,
     area INT,
@@ -13,52 +14,111 @@ CREATE TABLE Countries (
     gdpYear INT
 );
 
-ALTER TABLE Countries (name) Primary KEY;
-
 CREATE TABLE Borders (
-    name VARCHAR(20), name VARCHAR(20), name VARCHAR(20), name VARCHAR(20));
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) references Countries(name),
+    neighbor VARCHAR(20) references Countries(name));
 
 INSERT INTO Countries (
     name, latitude, longitude, area, population, gdp, gdpYear)
 Values(
-    "Germany", 51 00 N, 9 00 E, 357022, 80594017, 3.495 trillion, 3.979 trillion);
+    'Germany', 5100, 900, 357022, 80594017, 349500, 397900);
 
 INSERT INTO Countries (
     name, latitude, longitude, area, population, gdp, gdpYear)
 Values (
-    "Netherlands", 52 30 N, 5 45 E, 41543, 17084719, 773.9 billion, 870.8 billion);
+    'Netherlands', 5230, 545, 41543, 17084719, 773, 870);
 
 INSERT INTO Countries (
     name, latitude, longitude, area, population, gdp, gdpYear)
 Values (
-    "Belgium", 50 50 N, 4 00 E, 30528, 11491346, 470.2 billion, 508.6 billion);
+    'Belgium', 5050, 400, 30528, 11491346, 470, 508);
 
 INSERT INTO Countries (
     name, latitude, longitude, area, population, gdp, gdpYear)
 Values (
-    "Luxemburg", 49 45 N, 6 10 E, 2586, 594130, 60.98 billion, 58.74 billion);
+    'Luxemburg', 4945, 610, 2586, 594130, 60, 58);
 
 INSERT INTO Countries (
     name, latitude, longitude, area, population, gdp, gdpYear)
 Values (
-    "Poland", 52 00 N, 20 00 E, 312685, 38476269, 467.4 billion, 1.052 trillion);
+    'Poland', 5200, 2000, 312685, 38476269, 467, 105200);
 
 INSERT INTO Countries (
     name, latitude, longitude, area, population, gdp, gdpYear)
 Values (
-    "Czech Republic", 49 45 N, 15 30 E, 78867, 10674723, 193.5 billion, 350.9 billion);
+    'Czech Republic', 4945, 1530, 78867, 10674723, 193, 351);
 
 INSERT INTO Countries (
     name, latitude, longitude, area, population, gdp, gdpYear)
 Values (
-    "Austria", 47 20 N, 13 20 E, 83871, 8754413, 387.3 billion, 416.6 billion);
+    'Austria', 4720, 1320, 83871, 8754413, 387, 416);
 
 INSERT INTO Countries (
     name, latitude, longitude, area, population, gdp, gdpYear)
 Values (
-    "France", 46 00 N, 2 00 E, 643801, 67106161, 2.488 trillion, 2.699 trillion);
+    'France', 4600, 200, 643801, 67106161, 248800 , 269900 );
 
 INSERT INTO Countries (
     name, latitude, longitude, area, population, gdp, gdpYear)
 Values (
-    "Switzerland", 47 00 N, 8 00 E, 41277, 8236303, 662.5 billion, 496.3 billion);
+    'Switzerland', 4700, 800, 41277, 8236303, 662, 496);
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'Germany', 'Poland');
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'Germany', 'Czech Republic');
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'Germany', 'France');
+INSERT INTO Borders(name, neighbor)
+Values (
+    'Germany', 'Austria');
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'Germany', 'Belgium');
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'Germany', 'Luxemburg');
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'Germany', 'Netherlands');
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'Netherlands', 'Belgium');
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'Luxemburg', 'France');
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'Luxemburg', 'Belgium');
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'Poland', 'Czech Republic');
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'Austria', 'Czech Republic');
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'France', 'Switzerland');
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'France', 'Belgium');
+
+INSERT INTO Borders(name, neighbor)
+Values (
+    'Switzerland', 'Austria');
