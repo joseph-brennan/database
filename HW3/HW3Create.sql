20a0f16eae6efdc504c35d6f0eb8f62724fed10d
Joseph-Brennan <sharpshooter30@gmail.com>
2017-10-06 19:11:33 -0600

    think im done

--- HW3/HW3Create.sql
+++ HW3/HW3Create.sql
@@ -13,13 +13,14 @@ CREATE TABLE Regions (
 CREATE TABLE State (
     state_abv VARCHAR(5),
     fips INT,
-    state_nm VARCHAR(20) PRIMARY KEY
+    state_nm VARCHAR(20) PRIMARY KEY,
+    destination_wac INT
 );
 
 CREATE TABLE City (
-    city_state VARCHAR(40),
+    city VARCHAR(40),
     origin_market_id INT,
-    wac INT,
+    origin_wac INT,
     city_abr VARCHAR(10),
     PRIMARY KEY (city_abr)
 );
@@ -42,14 +43,10 @@ CREATE TABLE Aircrafts (
 );
 
 CREATE TABLE Airports (
-    destination_airport_id INT,
-    destination_airport_seq_id INT,
     origin_airport_id INT,
+    destination_airport_id INT,
     origin_airport_seq_id INT,
-    city_abr VARCHAR(10),
-    departures_scheduled INT,
-    departures_preformed INT,
-    FOREIGN KEY (city_abr) REFERENCES City(city_abr),
+    destination_airport_seq_id INT,
     PRIMARY KEY (origin_airport_id, destination_airport_id)
 );
 
@@ -57,11 +54,10 @@ CREATE TABLE Carrier (
     carrier_name VARCHAR(80),
     unique_carrier_name VARCHAR(80),
     carrier VARCHAR(10),
-    region_abr VARCHAR(10),
     carrier_group INT,
     carreir_group_new INT,
-    PRIMARY KEY (unique_carrier_name),
-    FOREIGN KEY (region_abr) REFERENCES Regions(region_abr)
+    carrier_entity VARCHAR(10),
+    PRIMARY KEY (carrier_entity)
 );
 
 CREATE TABLE Distance (
@@ -69,7 +65,8 @@ CREATE TABLE Distance (
     destination_airport_id INT,
     destance INT,
     destance_group INT,
-    PRIMARY KEY (origin_airport_id, destination_airport_id)
+    PRIMARY KEY (origin_airport_id, destination_airport_id),
+    FOREIGN KEY (origin_airport_id, destination_airport_id) REFERENCES Airports(origin_airport_id,destination_airport_id)
 );
 
 CREATE TABLE Flight (
@@ -87,6 +84,5 @@ CREATE TABLE Flight (
     aircraft_type INT,
     carrier VARCHAR(10),
     flight_id INT PRIMARY KEY,
-    FOREIGN KEY (origin_airport_id, destination_airport_id)
-    REFERENCES Airports(origin_airport_id, destination_airport_id)
+    FOREIGN KEY (origin_airport_id, destination_airport_id) REFERENCES Airports(origin_airport_id, destination_airport_id)
 );
