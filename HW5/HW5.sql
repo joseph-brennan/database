59b2fb295dcdc3c301e9db5a6d80bba2a963caf8
Joey <sharpshooter30@gmail.com>
2017-10-26 18:15:08 -0600

    finsihed i think

--- HW5/HW5.sql
+++ HW5/HW5.sql
@@ -1,43 +1,49 @@
 /*
 * Joey Brennan
-*
-*
+* \i 'C:/Users/Joey/Documents/Database/HW5/HW5.sql';
+* drop table booking, customer, mail_address;
 */
+CREATE TABLE Mail_Address (
+    my_key SERIAL PRIMARY KEY,
+    street VARCHAR(30),
+    city VARCHAR(30),
+    state VARCHAR(30),
+    postal_code INT
+);
+
+CREATE TABLE booking (
+    booking_number INT,
+    city_code INT,
+    book_date INT,
+    PRIMARY KEY (booking_number)
+);
 
 CREATE TABLE Customer (
     first_name VARCHAR(30),
     last_name VARCHAR(40),
-    mail_address VARCHAR(50),
-    phone_number VARCHAR(30),
-    email_address VARCHAR(50),
-    FOREIGN KEY mail_address REFERENCES mail_address(),
-    FOREIGN KEY phone_number REFERENCES phone_number(),
-    PRIMARY KEY
-);
-
-CREATE TABLE mail_address (
-    street VARCHAR(30),
-    city VARCHAR(30),
-    state VARCHAR(30),
-    postal_code INT,
-    PRIMARY KEY
-);
-
-CREATE TABLE phone_number (
-    country_code INT,
-    city_area_code INT,
-    local_number INT,
-    PRIMARY KEY
+    mail_address INT,
+    phone_number INT,
+    email_address_1 VARCHAR(30) UNIQUE,
+    FOREIGN KEY (mail_address) REFERENCES Mail_Address(my_key),
+    PRIMARY KEY (first_name, last_name)
 );
 
 CREATE TABLE flight (
     flight_origin INT,
     flight_destination INT,
-    flight_number INT,
+    unique_flight_number INT,
     airline INT,
     air_time INT,
     booking_number INT,
-    
-    FOREIGN KEY
-    PRIMARY KEY
+    flight_number INT,
+    date_time_origin INT,
+    date_time_destination INT,
+    customer_last_payment VARCHAR(30),
+    customer_first_payment VARCHAR(30),
+    customer_last_ticket VARCHAR(30),
+    customer_first_ticket VARCHAR(30),
+    FOREIGN KEY (booking_number) REFERENCES booking(booking_number),
+    FOREIGN KEY (customer_last_payment, customer_first_payment) REFERENCES Customer(last_name, first_name),
+    FOREIGN KEY (customer_last_ticket, customer_first_ticket) REFERENCES Customer(last_name, first_name),
+    PRIMARY KEY (unique_flight_number)
 );
